1b9a6f9101851aec0fb77c935696f77baf20270f
Andries Kruithof <andries.kruithof@nordicsemi.no>
2023-09-21 15:35:59 +0200

    basically working
    Signed-off-by: Andries Kruithof <andries.kruithof@nordicsemi.no>

--- src/main.rs
+++ src/main.rs
@@ -40,41 +40,53 @@ fn main() -> ! {
 	let mut new_value: u16 = 0;
 	let mut init_value: u16 = 0;
 
-	let image1 = [ [ 1, 0, 1, 0, 1],
-	[ 1, 0, 1, 0, 1],
-	[ 1, 0, 1, 0, 1],
-	[ 1, 0, 1, 0, 1],
-	[ 1, 0, 1, 0, 1], ];
+	let mut image1 = [ [ 1, 1, 1, 1, 1],
+	[ 1, 1, 1, 1, 1],
+	[ 1, 1, 1, 1, 1],
+	[ 1, 1, 1, 1, 1],
+	[ 1, 1, 1, 1, 1], ];
+	let mut image = image1;
+	// startup image, we also need a delay for the adc to initialise
 	display.show(&mut timer, image1, 100);
 
-init_value = saadc
-.read(&mut mic_in)
-.expect("Error") as u16;
+// we find the offset value, i.e. the background noise
+        init_value = saadc
+                     .read(&mut mic_in)
+                     .expect("Error") as u16;
+        init_value = init_value + 5;
         loop {
             let mic_value = saadc
                 .read(&mut mic_in)
                 .expect("could not read value of microphone") as u16;
 
             // Smoothen the signal as audio comes in waves
-            max_value = max_value.max(mic_value);
-
+            new_value = new_value.max(mic_value);
+            max_value = max_value.max(new_value);
             sum += mic_value as u64;
             count += 1;
 
             if count % 100 == 0 {
                 let avg = (sum / count) as u16;
-//		init_value = avg;
-		  new_value = max_value;
 
-                let image = [
-                    [if new_value > init_value + 100 { 1 } else { 0 }; 5],
-                    [if new_value >  init_value + 80 { 1 } else { 0 }; 5],
-                    [if new_value >  init_value + 60 { 1 } else { 0 }; 5],
-                    [if new_value >  init_value + 40 { 1 } else { 0 }; 5],
-                    [if new_value >  init_value + 20 { 1 } else { 0 }; 5],
-                ];
+// this should be done the rust way
+		for row in 0..5 {
+		for column in 1..5 {
+		image1[row][column] = image[row][column-1];
+		}
+		}
+
+// this should be done the rust way
+               image = image1;
+	       let range = 5; //  (max_value - init_value)/5;
+	       if (new_value > init_value + 16*range) {image[0][0] = 1} else {image[0][0] = 0}
+	       if (new_value > init_value + 8*range) {image[1][0] = 1} else {image[1][0] = 0}
+	       if (new_value > init_value + 4*range) {image[2][0] = 1} else {image[2][0] = 0}
+	       if (new_value > init_value + 2*range) {image[3][0] = 1} else {image[3][0] = 0}
+	       if (new_value > init_value + range) {image[4][0] = 1} else {image[4][0] = 0}
+
+
                 display.show(&mut timer, image, 10);
-                max_value = 0;
+                new_value = 0;
             }
         }
     }
